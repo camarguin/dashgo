7a7899f7b52abe5558e06aea4f954463f8464a76
Lucas Gerhardt de Camargo <lucasgcamargo@icloud.com>
2021-08-24 11:26:00 -0700

    Added responsive nav

--- src/components/Header/NotificationsNav.tsx
+++ src/components/Header/NotificationsNav.tsx
@@ -4,7 +4,15 @@ import { RiNotificationLine, RiUserAddLine } from 'react-icons/ri'
 
 const NotificationsNav = () => {
   return (
-    <HStack spacing="8" mx="8" borderRightWidth={1} borderColor="gray.700" pr="8" py="1" color="gray.300">
+    <HStack
+      spacing={["6", "8"]}
+      mx={["6", "8"]}
+      borderRightWidth={1}
+      borderColor="gray.700"
+      pr={["6", "8"]}
+      py="1"
+      color="gray.300"
+    >
       <Icon as={RiNotificationLine} fontSize="20" />
       <Icon as={RiUserAddLine} fontSize="20" />
     </HStack>
--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export default function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export default function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Lucas Gerhardt</Text>
-        <Text color="gray.300" fontSize="small">
-          lucasgcamargo@icloud.com
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Lucas Gerhardt</Text>
+          <Text color="gray.300" fontSize="small">
+            lucasgcamargo@icloud.com
+          </Text>
+        </Box>
+      )}
       <Avatar size="md" name="Lucas Gerhardt" src="https://github.com/camarguin.png" />
     </Flex>
   )
