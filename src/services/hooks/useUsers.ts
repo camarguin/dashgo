e9e4207161edf041e49253f6b7eae86a819f816d
Lucas Gerhardt de Camargo <lucasgcamargo@icloud.com>
2021-08-30 10:41:37 -0700

    Deleted congi.ts"

--- src/services/hooks/useUsers.ts
+++ src/services/hooks/useUsers.ts
@@ -1,24 +1,33 @@
 import { useQuery } from "react-query";
 import { api } from "../api";
 
-export function useUsers() {
-  return useQuery('users', async () => {
-    const { data } = await api.get('users')
+type User = {
+  id: string;
+  name: string;
+  email: string;
+  createdAt: string;
+}
 
-    const users = data.users.map(user => {
-      return {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        createdAt: new Date(user.createdAt).toLocaleDateString('en-CA', {
-          day: '2-digit',
-          month: 'long',
-          year: 'numeric',
-        })
-      }
-    });
-    return users;
-  }, {
+export async function getUsers(): Promise<User[]> {
+  const { data } = await api.get('users')
+
+  const users = data.users.map(user => {
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      createdAt: new Date(user.createdAt).toLocaleDateString('en-CA', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric',
+      })
+    }
+  });
+  return users;
+}
+
+export function useUsers() {
+  return useQuery('users', getUsers, {
     staleTime: 5000,
   })
 }
